19988027d5d75b4fca3e6ffe8ffa680736b861b9
John Ikhanoba <oshioi@yahoo.co.uk>
2022-09-09 05:47:52 -0700

    new commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char a[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(a[i]);
+	}
+	putchar('\n')
+	return (0);
+}
