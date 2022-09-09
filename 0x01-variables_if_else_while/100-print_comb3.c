88b53cddcb9395871d64e5d7822df678374e03a3
John Ikhanoba <oshioi@yahoo.co.uk>
2022-09-09 08:03:19 -0700

    new commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -16,15 +16,15 @@ int main(void)
 		i = 48;
 		while (i < 58)
 		{
-			putchar(e):
-			putchar(i):
+			putchar(e);
+			putchar(i);
 			if (i == 57 && e == 57)
 		{
 			break;
 		}
 			putchar(',');
 			putchar(' ');
-			i++
+			i++;
 		}
 		e++;
 	}
