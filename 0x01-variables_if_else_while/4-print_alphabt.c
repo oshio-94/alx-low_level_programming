d2bd13f6727f4fc86c9970bc4231abe1a4225616
John Ikhanoba <oshioi@yahoo.co.uk>
2022-09-09 06:06:30 -0700

    new commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		if (i != 113 && i != 101)
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
