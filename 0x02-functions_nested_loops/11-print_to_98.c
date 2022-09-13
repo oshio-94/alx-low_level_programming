9a0323ab3665166b3fc33e092bbe17638c63f2ca
John Ikhanoba <oshioi@yahoo.co.uk>
2022-09-13 08:42:24 -0700

    twelveth commit

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_to _98 - Prints all natural numbers from x to 98
+ *
+ * @x: input number
+ *
+ * Return: no return
+ */
+void print_to_98(int x)
+{
+	if (x > 98)
+	{
+		for (x = x; x <= 97; x++)
+			printf("%d, ", x);
+	}
+	else
+		for (x = x; x > 98; x--)
+			printf("%d, ", x);
+	printf("98\n");
+
