5b8994ed333b7ca6c5f7600139222b073f429502
John Ikhanoba <oshioi@yahoo.co.uk>
2022-09-13 07:32:51 -0700

    eight commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_last_digit - Prints the last digit
+ *
+ * @n: input number of an integer
+ *
+ * Return: last digit
+ */
+int print_last_digit(int n)
+{
+	int l;
+
+	l = n % 10;
+	if (1 < 0)
+	{
+		_putchar(-l + 48);
+		return (-l);
+	}
+	else
+	{
+		_putchar(l + 48);
+		return (l);
+	}
+}
