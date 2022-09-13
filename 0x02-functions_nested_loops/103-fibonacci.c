5fb6f3932c07f5f7f191409bdb9cee32e18a035d
John Ikhanoba <oshioi@yahoo.co.uk>
2022-09-13 10:27:36 -0700

    new commits

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Prints the add the even-valued fibonacci numbers
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	long int a, b, c, d;
+	
+	a = 1;
+	b = 2;
+	c = d = 0;
+	while (c <= 4000000)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+		if ((a % 2) == 0)
+		{
+			d += a;
+		}
+	}
+	printf("%ld\n", d);
+	return (0);
+}
