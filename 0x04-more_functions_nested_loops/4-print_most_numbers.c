9f9f70eb815988ec4a673611b9f4e180891e72e8
John Ikhanoba <oshioi@yahoo.co.uk>
2022-09-15 05:20:37 -0700

    fifth commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,4 +5,15 @@
  * Return: no return
  */
 void print_most_numbers(void)
+{
+	int c;
 
+	for (c = 48; c < 58; c++)
+	{
+		if (c != 50 && c != 52)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
