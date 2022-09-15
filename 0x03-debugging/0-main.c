9a79cea2a9ad66e06cef4b4dbde63eeffc67dded
John Ikhanoba <oshioi@yahoo.co.uk>
2022-09-15 11:01:40 -0700

    new commit

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * main - Tests functions that prints if integer is positive or negative
+ * main - tests functions that prints if integer is positive or negative
  *
  * Return: 0
  */
