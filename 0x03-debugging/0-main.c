1a86c1809f27c7b3e3ab07edd95c6ebc59fbed74
John Ikhanoba <oshioi@yahoo.co.uk>
2022-09-14 23:53:02 -0700

    new corrections

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,16 @@
+#include<stdio.h>
 #include "main.h"
 /**
  * main - Tests functions that prints if integer is positive or negative
  *
  * Return: 0
  */
+
 int main(void)
 {
 	int i;
-
+	
 	i = 0;
 	positive_or_negative(i);
-
 	return (0);
 }
