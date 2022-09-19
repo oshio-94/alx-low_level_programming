febf899c8b4db1774eea4e6e27d14c8ac274de44
John Ikhanoba <oshioi@yahoo.co.uk>
2022-09-19 04:08:52 -0700

    corrections

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * point_array - prints n  elements of an array of ints
+ * print_array - prints n  elements of an array of ints
  * @a: input array
  * @n: input n elements
  * Return: no return
