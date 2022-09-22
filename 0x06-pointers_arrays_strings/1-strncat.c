1d380aa140947dfcb8c864384a5d903a946853fa
John Ikhanoba <oshioi@yahoo.co.uk>
2022-09-22 04:29:44 -0700

    corrections

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -6,7 +6,7 @@
  * @n: amount of bytes used from src
  * Return: the pointer to dest
  */
-char *_strncat(char *dest,char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
 	int count , count1;
 
