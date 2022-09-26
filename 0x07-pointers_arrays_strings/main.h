933bb5077e7d6c6ba53c2db26ffa391e3c7d8b9b
John Ikhanoba <oshioi@yahoo.co.uk>
2022-09-26 08:57:52 -0700

    third commit

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -50,4 +50,5 @@ void print_buffer(char *b, int size);
 void print_number(int n);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
 #endif
