efabbb1e51e2f7c34a612a1cd3549220da245f36
John Ikhanoba <oshioi@yahoo.co.uk>
2022-09-30 06:10:58 -0700

    fifth commit

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -5,13 +5,13 @@
  * main - adds positive numbers
  * @argc: number of command line arguments
  * @argv: array that contains the program command line arguments
- * Return : 0 for success
+ * Return: 0 for success
  */
 int main(int argc, char *argv[])
 {
 	int i, j, add = 0;
 
-	for (i = i; i < argc; i++)
+	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
