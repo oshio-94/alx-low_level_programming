7ce76dd9bde10122f7575e38851d430af9d74eee
John Ikhanoba <oshioi@yahoo.co.uk>
2022-09-30 04:39:21 -0700

    first commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - program that prints its name 
+ * @argc: argument count
+ * @argv: argument value, a string that come after calling function
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+		printf("%s\n", argv[0]);
+
+	return (0);
+}
