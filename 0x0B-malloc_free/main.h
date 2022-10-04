8d9d295418dd2771fd1023692c33c77481ca3afa
John Ikhanoba <oshioi@yahoo.co.uk>
2022-10-04 03:53:38 -0700

    third task

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -67,4 +67,5 @@ int is_prime_number(int n);
 int wildcmp(char *s1, char *s2);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
 #endif
