131fb477b464fb86bdc883e88f2436f1c4b21fd1
John Ikhanoba <oshioi@yahoo.co.uk>
2022-10-20 09:36:24 -0700

    new commit

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+/*******constructors*********/
+void adv_links(void) __attribute__((constructor));
 /**
  * linklist - executed before main 
  * Return - no value
  */
-void adv_links ()
+void adv_links(void)
 {
-	printf("You're beat! and yet, you must allow");
-	printf(",\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
