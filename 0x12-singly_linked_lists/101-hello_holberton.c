4d144803d8f995b1740cf39674406936e425a6b3
John Ikhanoba <oshioi@yahoo.co.uk>
2022-10-20 08:57:19 -0700

    new tasks

--- 0x12-singly_linked_lists/101-hello_holberton.c
+++ 0x12-singly_linked_lists/101-hello_holberton.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
  * main - function that prints Hello world
- * Return - no return
+ * Return: returns 0 for success
  */
 int main(void)
 {
 	printf("Hello, Holberton\n");
+	return (0);
 }
