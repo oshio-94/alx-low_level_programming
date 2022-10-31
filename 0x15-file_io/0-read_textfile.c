5a989e350d4c59d550b9261977c35b52466ff2a0
John Ikhanoba <oshioi@yahoo.co.uk>
2022-10-31 10:04:35 -0700

    new corrections

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,8 +2,8 @@
 /**
  * read_textfile - function that reads a text file and prints the letters
  * @filename: filename.
- * @letters: numbers of letters printed. 
- * Return: numbers of letters printed. It fails, returns 0. 
+ * @letters: numbers of letters printed.
+ * Return: numbers of letters printed. It fails, returns 0
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
